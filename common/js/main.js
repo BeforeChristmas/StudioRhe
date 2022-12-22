b3ac8d8ab8ff02e72e47f2287845d3463e6b2e4e
ThioKDG <kim2006207@gmail.com>
2022-12-22 11:18:18 +0900

    플러그인 삭제 및 페이지 첫 화면? 로딩? 이미지 생성

--- common/js/main.js
+++ common/js/main.js
@@ -1,17 +1,19 @@
-/* locomotive js */
-// import LocomotiveScroll from './node_modules/locomotive-scroll/dist/locomotive-scroll.js';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
-
 /* 메인최상위 선언 및 하위 컨텐츠 박스 선언 */
 const container = document.querySelector('#mainWrapper');
-const boxes = document.querySelectorAll('#mainWapper section');
+const boxes = document.querySelectorAll('#FirstContainer');
+/* 페이지 로딩 영역 선언 */
+const pScreen = document.querySelector('.pageScreen');
+
 
 /* 가로스크롤 이벤트 */
 const scrollLeft = (e) =>{
     container.scrollLeft += e.deltaY;
 }  
 container.addEventListener('wheel', scrollLeft)
+
+/* 페이지 로딩 스크린 이벤트 */
+setTimeout(() => {
+   pScreen.style.display ='none' 
+}, 1100);
+
+/* 첫번째 섹션 */
