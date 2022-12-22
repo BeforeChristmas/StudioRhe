27495bca5aea45e259edb1c68de138ee0eac82dc
ThioKDG <kim2006207@gmail.com>
2022-12-22 09:08:22 +0900

    20~21 작업물

--- common/js/main.js
+++ common/js/main.js
@@ -1,3 +1,11 @@
+/* locomotive js */
+// import LocomotiveScroll from './node_modules/locomotive-scroll/dist/locomotive-scroll.js';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
 /* 메인최상위 선언 및 하위 컨텐츠 박스 선언 */
 const container = document.querySelector('#mainWrapper');
 const boxes = document.querySelectorAll('#mainWapper section');
