b358eaef9d26618dddeabf9df2ec29c408668c3c
ThioKDG <kim2006207@gmail.com>
2022-12-22 17:59:49 +0900

    22 작업물 추가

--- common/js/main.js
+++ common/js/main.js
@@ -1,19 +1,47 @@
 /* 메인최상위 선언 및 하위 컨텐츠 박스 선언 */
+const box = document.querySelector('.scrollBox');
 const container = document.querySelector('#mainWrapper');
-const boxes = document.querySelectorAll('#FirstContainer');
+const fstcontainer = document.querySelector('#FirstContainer');
+const sndcontainer = document.querySelector('#secondContainer');
+const trdcontainer = document.querySelector('#thirdContainer');
+const fthcontainer = document.querySelector('#fourthContainer');
 /* 페이지 로딩 영역 선언 */
 const pScreen = document.querySelector('.pageScreen');
-
+/* 헤더 영역 선언 */
+const haeder = document.querySelector('.headerLeft');
+const logoChange = document.querySelector('.Head');
+const btn = document.querySelector('.HeaderBtn');
+const logoArea = document.querySelector('#logoContainer');
 
 /* 가로스크롤 이벤트 */
 const scrollLeft = (e) =>{
-    container.scrollLeft += e.deltaY;
+    let scrLeft = container.scrollLeft += e.deltaY;
+    console.log(scrLeft);
+
+    if(scrLeft > 100){
+        logoArea.style.opacity = '0'
+    } else {
+        logoArea.style.opacity = '1'
+    }
 }  
 container.addEventListener('wheel', scrollLeft)
+
+// function delht(e){
+//     let deltaht = 0;
+//     deltaht = deltaht + e.deltaY
+// }
+// box.addEventListener('wheel', (e) =>{
+//     container.style.transform = `translateX(-${fthcontainer.offsetLeft += e.deltaY}px)`;
+// });
 
 /* 페이지 로딩 스크린 이벤트 */
 setTimeout(() => {
    pScreen.style.display ='none' 
 }, 1100);
 
-/* 첫번째 섹션 */
+/* 헤더 영역 클릭시 off 클래스 추가 */
+btn.addEventListener('click', e =>{
+    e.preventDefault();
+    haeder.classList.toggle('active');
+});
+
